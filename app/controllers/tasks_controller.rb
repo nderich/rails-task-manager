c29a714c0b483baf81e0c64e5da6d59edad8d8af
Nicolas Derichsweiler <nicolasderichsweiler@gmail.com>
2019-11-13 11:29:22 +0100

    unfinished_business

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,29 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
